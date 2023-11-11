f87d79c4bfda5bf5c6a0bbac198e4d7039c6dc37
BilalHammi <Bilalhammi45@gmail.com>
2023-11-11 11:12:56 +0100

    HTML almost done + added JS function

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputID = document.getElementById("Todo");
+const buttonID = document.getElementById("Knop");
+const divID = document.getElementById("mainDiv");
+let counterDiv = 0;
+
+function Todo() {
+    let woord = inputID.value;
+    if (woord.length > 0 && 25 > woord.length) {
+        const newDiv = document.createElement("div");
+        if (6 > counterDiv) {
+            newDiv.classList.add('bg-black', 'h-14', 'w-[28rem]', 'my-5', 'mx-5', 'rounded-lg');
+            console.log(divID.appendChild(newDiv));
+            counterDiv++;
+        }
+    } else {
+        console.log("Dat kan niet gozer!");
+    }
+    console.log(counterDiv);
+};
+
+buttonID.addEventListener("click", Todo);
