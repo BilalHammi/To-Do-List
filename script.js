7a3b4ca20c53e05dda68765eff0787b1d2e3d9dc
BilalHammi <Bilalhammi45@gmail.com>
2023-12-02 15:57:30 +0100

    Fixed Button form issue.

--- script.js
+++ script.js
@@ -77,22 +77,37 @@ function buttonTagHover(input) {
 }
 
 function Form() {
-    const buttonArray = [];
-    const buttonForm = document.createElement("button");
-    const buttonStyle = ['bg-gray-500', 'rounded-md', 'text-white', 'font-bold', 'py-5', 'px-20', 'mx-3', 'mb-2', 'hover:bg-red-500'];
-    formDiv.setAttribute("id", "formdiv");
 
+    formDiv.setAttribute("id", "formdiv");
     formDiv.classList.add('bg-white', 'h-[26rem]', 'w-96', 'z-40', 'rounded-lg', 'absolute', 'bottom-72', 'ml-14', 'flex', 'items-end', 'justify-center');
 
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 1; i++) {
+        const buttonForm = document.createElement("button");
+        const buttonStyle = ['bg-gray-500', 'rounded-md', 'text-white', 'font-bold', 'py-4', 'px-12', 'mx-3', 'mb-3', 'hover:bg-red-500'];
+
         buttonStyle.forEach(style => {
             buttonForm.classList.add(style);
         });
+
+        idCounter0++;
+
+        buttonForm.setAttribute('id', `button_${idCounter0}`);
+
+        formDiv.appendChild(buttonForm);
+
+        const buttonformID2 = document.getElementById(`button_${idCounter0}`);
+        const buttonformID3 = document.getElementById(`button_${idCounter0}`);
+
+        if (idCounter0 === 2) {
+            buttonformID2.innerHTML = 'Cancel';
+        } else {
+            buttonformID3.innerHTML = 'Confirm';
+        }
+
+
     }
-    buttonArray.push(buttonForm);
-    formDiv.appendChild(buttonForm)
-    console.log(buttonArray);
 }
+
 
 function blurselector() {
     const allDiv = document.querySelectorAll("div");
